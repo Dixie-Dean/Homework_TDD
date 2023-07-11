ea7463ce34ae7b7c22ed1f70d8ad2bb1e5a933a6
Dixie Dean <d.w.sokolov26@gmail.com>
2023-07-11 12:44:02 +0300

    Add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,11 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
+    private final Map<String, Integer> contacts = new TreeMap<>();
+
     public int add(String name, int number) {
-        return 0;
+        contacts.put(name, number);
+        return contacts.size();
     }
 }
